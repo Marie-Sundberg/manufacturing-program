8ea51fd29b11f12d224c5d548fe874b2ccbb861f
Marie Sundberg <lia2@viktorstahl.com>
2023-05-17 17:18:02 +0200

    klar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Firestore, collection, addDoc, collectionData, doc, updateDoc, deleteDoc } from '@angular/fire/firestore';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Observable, empty } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ResultData } from './classes/result-data';
 
 @Component({
@@ -18,12 +17,12 @@ export class AppComponent {
    });
 
   userData$!: Observable<any>;
-  effect!: number;
-  voltage!: number;
+  p!: number;
+  v!: number;
   heatedLength!: number;
   numberOfLaps!: number;
-  utdragResultat!: number;
-  okResultat: Array<ResultData> = [];
+  u!: number;
+  okResults: Array<ResultData> = [];
   sortedList: Array<ResultData> = [];
   yCoupled: boolean = false;
   showText: boolean = false;
@@ -32,7 +31,7 @@ export class AppComponent {
     {diameter: 0.200, ohm: 42.97},
     {diameter: 0.260, ohm: 25.43},
     {diameter: 0.265, ohm: 24.48},
-    {diameter: 0.300, ohm: 19.1},
+    {diameter: 0.300, ohm: 19.10},
     {diameter: 0.335, ohm: 15.32},
     {diameter: 0.375, ohm: 12.23},
     {diameter: 0.420, ohm: 9.74},
@@ -47,44 +46,43 @@ export class AppComponent {
     {diameter: 0.800, ohm: 2.69},
     {diameter: 0.850, ohm: 2.38},
     {diameter: 0.900, ohm: 2.12},
-    {diameter: 0.950, ohm: 1.9}
+    {diameter: 0.950, ohm: 1.90}
   ]
 
   dornArray: number[] = [1.7, 2.0, 2.5, 2.8, 3.0, 3.2, 3.5, 3.8, 4.0, 5.0, 6.0, 6.5, 7.0];
- 
-  constructor(private firestore: Firestore) {
-    this.getData();
-  }
 
   addOrder() {
-    this.okResultat = [];
-    this.effect = this.countForm.value.effect;
-    this.voltage = this.countForm.value.voltage;
+    this.okResults = [];
+    this.p = this.countForm.value.effect;
+    this.v = this.countForm.value.voltage;
     this.heatedLength = this.countForm.value.heatedLength;
-
-    const pipeLength = (this.heatedLength *2) -120;
-    const resistance = Math.pow(this.voltage,2) / (this.effect);
+    const lK = (this.heatedLength *2)-120;
+    //rT från papperet = rLind här nere 
+    const rLind = Math.pow(this.v,2) / (this.p);
+    const r20 = rLind / 1.03;
 
     for (let index = 0; index < this.dornArray.length; index++) {
       this.wireArray.forEach(wire => {
-        const lapLength = (wire.diameter + this.dornArray[index]) * Math.PI;
-        const lWire = resistance / wire.ohm;
-        this.numberOfLaps = (lWire * 1000) / lapLength;
+        const o = (wire.diameter + this.dornArray[index]) * Math.PI;
+        const lWire = r20 / wire.ohm;
+        this.numberOfLaps = (lWire * 1000) / o;
         const lTät = (wire.diameter) * this.numberOfLaps;
-        this.utdragResultat =  (pipeLength / lTät);
-        const surfacePressure = this.effect / 10 / wire.diameter / Math.PI / lWire;
-        const surfacePressureCartridge = this.effect / (pipeLength/10 * 3.77);
-        if(this.utdragResultat > 2.9 && this.utdragResultat < 3.2 && surfacePressure < 15){
-          const rounded = Math.round(this.numberOfLaps /2);
-          const okObjects = new ResultData(Math.round(resistance * 100)/100, rounded, wire.diameter, this.dornArray[index], Math.round(this.utdragResultat*100)/100, Math.round(surfacePressure * 100)/100, pipeLength/2, Math.round(surfacePressureCartridge * 100)/100);
-          this.okResultat.push(okObjects);
+        this.u = (lK / lTät);
+        const surfacePressure = this.p / 10 / wire.diameter / Math.PI / lWire;
+        const surfacePressureCartridge = this.p / (lK/10 * 3.77);
+        if(this.u > 2.9 && this.u < 3.3 && surfacePressure < 15){
+          const upperLimit = r20 * (1 + 0.015);
+          const lowerLimit = r20 * (1 - 0.015);
+          const roundedNumberOfLaps = Math.round(this.numberOfLaps /2);
+          const okObjects = new ResultData(Math.round(r20 * 100)/100, roundedNumberOfLaps, wire.diameter, this.dornArray[index], Math.round(this.u*100)/100,
+          Math.round(surfacePressure * 100)/100, lK/2, Math.round(surfacePressureCartridge * 100)/100, Math.round(upperLimit * 10)/10, Math.round(lowerLimit * 10)/10);
+          this.okResults.push(okObjects);
           this.showText = true;
           return;
         }
-
       });
     }
-    switch (this.voltage) {
+    switch (this.v) {
       case 230:
         this.yCoupled = true;
         break;
@@ -92,47 +90,6 @@ export class AppComponent {
         this.yCoupled = false
       break;
     }
-    //this.sortedList = this.okResultat.sort((a,b) => Math.abs(a-3)-Math.abs(b-3)); /* visar den som är närmast 3.0 */
-    this.sortedList = this.okResultat.sort((a, b) => a.surfacePressure - b.surfacePressure);
-  }
-
-  addData(form:any) {
-    const collectionInstance = collection(this.firestore, 'users');
-    addDoc(collectionInstance, form.value).then(() => {
-      console.log('Data Saved Successfully');
-    }).catch((error) => {
-      console.log(error);
-    })
-  }
-
-  getData() {
-    const collectionInstance = collection(this.firestore, 'users');
-    collectionData(collectionInstance, { idField: 'id'}).subscribe(val => {
-      //console.log(val);
-    })
-
-    this.userData$ = collectionData(collectionInstance, { idField: 'id'});
-  }
-
-  updateData(id: string) {
-    const docInstance = doc(this.firestore, 'users', id);
-    // ändra detta till ett form som man fyller i sen så det blir rätt.
-    const updateData = {
-      name: 'updatedName'
-    }
-    updateDoc(docInstance, updateData)
-    .then(() => {
-      console.log('Data Updated');
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
-
-  deleteData(id:string) {
-    const docInstance = doc(this.firestore, 'users', id);
-    deleteDoc(docInstance).then(() => {
-      console.log('Data Deleted');
-    })
+    this.sortedList = this.okResults.sort((a, b) => a.surfacePressure - b.surfacePressure);
   }
 }
--- src/app/classes/result-data.ts
+++ src/app/classes/result-data.ts
@@ -2,21 +2,25 @@ export class ResultData {
     wireDiameter: number;
     dornDiameter: number;
     numberOfLaps: number;
-    resistance: number;
+    rLind: number;
     u: number;
     surfacePressure: number;
-    pipeLength: number;
+    lK: number;
     surfacePressureCartridge: number;
+    upperLimit: number;
+    lowerLimit: number;
 
-
-    constructor(resistance: number, numberOfLaps: number, wireDiameter: number, dornDiameter: number, u:number, surfacePressure:number, pipeLength:number, surfacePressureCartridge:number) {
-        this.resistance = resistance;
+    constructor(rLind: number, numberOfLaps: number, wireDiameter: number, dornDiameter: number,
+      u:number, surfacePressure:number, lK:number, surfacePressureCartridge:number, upperLimit: number, lowerLimit: number) {
+        this.rLind = rLind;
         this.numberOfLaps = numberOfLaps;
         this.wireDiameter = wireDiameter;
         this.dornDiameter = dornDiameter;
         this.u = u;
         this.surfacePressure = surfacePressure;
-        this.pipeLength = pipeLength;
+        this.lK = lK;
         this.surfacePressureCartridge = surfacePressureCartridge;
+        this.upperLimit = upperLimit;
+        this.lowerLimit = lowerLimit;
       }
 }
